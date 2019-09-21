d0c7f8f9ee3fcfcae6c6c8897f23e414cd1ad606
Stewart Taylor <stewart.taylor@outlook.com>
2019-09-21 19:01:19 +0100

    added build folders for CI

---
+++ build/csharp-http-util/function/FunctionHandler.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Function
+{
+    public class FunctionHandler
+    {
+        private static readonly HttpClient _httpClient = new HttpClient();
+
+        public Task<string> Handle(string input)
+        {
+            var request = JsonConvert.DeserializeObject<QueryRequest>(input);
+            if (request == null)
+            {
+                return Task.FromResult($"Error input");
+            }
+
+            var result = GetStatusForUrl(request.Url).Result;
+            return Task.FromResult(JsonConvert.SerializeObject(result));
+        }
+
+        private async Task<QueryResult> GetStatusForUrl(string url)
+        {
+            var result = new QueryResult();
+
+            var httpResult = await _httpClient.GetAsync(url);
+
+            result.FirstStatus = httpResult.StatusCode;
+
+            if (httpResult.Content != null)
+            {
+                result.ContentLength = httpResult.Content.ToString().Length;
+            }
+            else
+            {
+                result.ContentLength = -1;
+            }
+
+            return result;
+        }
+    }
+
+    internal class QueryRequest
+    {
+        public string Url { get; set; }
+    }
+
+    internal class QueryResult
+    {
+        public HttpStatusCode FirstStatus { get; set; }
+        public int FinalStatus { get; set; }
+        public int ContentLength { get; set; }
+        public int Hops { get; set; }
+    }
+}
